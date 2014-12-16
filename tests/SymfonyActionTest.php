720a02268a3b04331e7b7c7617f632e7e8bd16c5
pitpit <damien.pitard@digitaslbi.fr>
2014-12-16 21:32:47 +0100

    testing all SymfonyAction methods

--- tests/SymfonyActionTest.php
+++ tests/SymfonyActionTest.php
@@ -6,13 +6,55 @@ use Creads\Api2Symfony\SymfonyAction;
 
 class SymfonyActionTest extends \PHPUnit_Framework_TestCase
 {
+    protected function getMockedRoute()
+    {
+        return $this->getMock('Creads\Api2Symfony\SymfonyRoute', array(), array('/route_path', 'route_name'));
+    }
+
+    protected function getMockedResponse()
+    {
+        return $this->getMock('Creads\Api2Symfony\SymfonyResponse', array(), array('/route_path', 'route_name', 'application\json'));
+    }
+
+    public function testConstruct()
+    {
+        $route = $this->getMockedRoute();
+        $action = new SymfonyAction('myname', $route, 'get', 'my description');
+
+        $this->assertEquals('myname', $action->getName());
+        $this->assertSame($route, $action->getRoute());
+        $this->assertEquals('get', $action->getMethod());
+        $this->assertEquals('my description', $action->getDescription());
+        $this->assertEquals(array(), $action->getParameters());
+    }
+
+    public function testAddParameterAndGetParameters()
+    {
+        $route = $this->getMockedRoute();
+        $action = new SymfonyAction('myname', $route, 'get', 'my description');
+        $action->addParameter('my-parameter');
+
+        $this->assertEquals(array('my-parameter'), $action->getParameters());
+    }
+
+    public function testSetResponseAndGetResponse()
+    {
+        $route = $this->getMockedRoute();
+        $action = new SymfonyAction('myname', $route, 'get', 'my description');
+        $response = $this->getMockedResponse();
+        $action->setResponse($response);
+
+        $this->assertSame($response, $action->getResponse());
+    }
+
     /**
      * @expectedException           Exception
      * @expectedExceptionMessage    You must provide a action's name
      */
     public function testNoName()
     {
-        new SymfonyAction('', $this->getMock('Creads\Api2Symfony\SymfonyRoute', array(), array(), '', false), 'get');
+        $route = $this->getMockedRoute();
+        new SymfonyAction('', $route, 'get');
     }
 
     /**
@@ -21,6 +63,7 @@ class SymfonyActionTest extends \PHPUnit_Framework_TestCase
      */
     public function testNoMethod()
     {
-        new SymfonyAction('test', $this->getMock('Creads\Api2Symfony\SymfonyRoute', array(), array(), '', false), '');
+        $route = $this->getMockedRoute();
+        new SymfonyAction('test', $route, '');
     }
 }
